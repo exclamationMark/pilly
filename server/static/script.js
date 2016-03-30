dd424c96fef086c7797a7deed60aeeddfd4c0bd2
Davide <dabongio93@yahoo.it>
2016-03-30 18:24:13 +0200

    Converted the table to divs, so it animates nicely. Added javascript function to insert a new table row (newElement(timestamp, eventname) ), try it out by clicking on the pill number.

--- server/static/script.js
+++ server/static/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
 	var w = $('.menu').width();
 	$('.menu').css({left : -w});
+	$('.pillnumber').click(function() {
+		newEvent('dummy1','dummy2');
+	});
+
+	$('.table-row:even').css("background-color", "#A1BABA");
 });
 
 function openMenu() {
@@ -16,3 +21,18 @@ function closeMenu() {
 		$('.overlay').css({display : 'none'});
 	});
 }
+
+function newEvent(timestamp, eventstring) {
+	var htmlString = '<div class="table-row">\
+						<div class="table-cell">' + timestamp + '</div>\
+						<div class="table-cell">' + eventstring + '</div>\
+					</div>';
+	var newElement = $(htmlString).css({display : 'none'});
+	newElement.css({backgroundColor : $('.table-row:eq(1)').css('background-color')});
+	$('.table-row').first().before(newElement);
+	$('.table-row').first().slideDown();
+	$('.table-row').last().slideUp(function() {
+		$('.table-row').last().remove();
+	});
+}
+

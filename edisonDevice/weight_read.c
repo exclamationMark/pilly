cc17b71c4c2f8873779d1fc1a1020759d123ec15
pk23 <pkupsc@stanford.edu>
2016-05-01 16:20:58 +0200

    Somewhat reliable weight_read
    reads 11 valid (not all ones) values and takes the median.

--- edisonDevice/weight_read.c
+++ edisonDevice/weight_read.c
@@ -1,4 +1,5 @@
 #include "mraa.h"
+#define n_reads 11
 
 void fuckDelay(int time){
 	int i=0;
@@ -11,16 +12,21 @@ void fuckDelay(int time){
 
 int main(int argc, char **argv)  
 {  
-	int i, output;
-    fprintf(stdout, "hello mraa\n Version: %s\n", mraa_get_version());  
+	int i, j, output, outputArray[n_reads], i_reads, tmp;
+ 
     mraa_gpio_context clk = mraa_gpio_init(4); 
     mraa_gpio_context dat = mraa_gpio_init(2);
     mraa_gpio_dir(clk, MRAA_GPIO_OUT); 
     mraa_gpio_dir(dat, MRAA_GPIO_IN); 
   
-    mraa_gpio_use_mmaped(clk, 1);  
-  
-    for (;;) { 
+    mraa_gpio_use_mmaped(clk, 1);
+
+    i_reads = n_reads;
+
+    while (i_reads > 0) { 
+
+        while(mraa_gpio_read(dat)) {}
+
     	output = 0; 
         for(i = 0; i < 24; i++) {
         	mraa_gpio_write(clk, 1);
@@ -32,8 +38,28 @@ int main(int argc, char **argv)
         mraa_gpio_write(clk, 1);
         fuckDelay(2000);
         mraa_gpio_write(clk, 0);
-        fuckDelay(10000000);
-        if(output < 16777215)
-        	fprintf(stdout, "%d\n", output);  
-    }  
+
+        if(output < 16777215) {
+        	//fprintf(stdout, "%d\n", output);
+            outputArray[i_reads-1] = output;
+            i_reads--; // decrement only if the valid read
+        }
+    } 
+
+
+    //sorting the array
+    for(i = 1; i < n_reads; i++){
+        j = i;
+        while ((j > 0) && (outputArray[j-1]>outputArray[j])) {
+            tmp = outputArray[j];
+            outputArray[j] = outputArray[j-1];
+            outputArray[j-1] = tmp;
+            j--;
+        }
+    } 
+
+    fprintf(stdout, "%d\n", outputArray[5]);
+
 } 
+
+

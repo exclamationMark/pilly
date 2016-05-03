e04fbf21e573e176b7386150796dadc55f8b145a
Davide <dabongio93@yahoo.it>
2016-05-03 18:00:49 +0200

    All info is now shown on the status Activity

--- AndroidApp/Pilly/app/src/main/java/co/pilly/pilly/Status.java
+++ AndroidApp/Pilly/app/src/main/java/co/pilly/pilly/Status.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class Status extends AppCompatActivity {
 
     @Override
@@ -16,22 +19,36 @@ public class Status extends AppCompatActivity {
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Typeface light = Typeface.createFromAsset(this.getAssets(), "Roboto-Thin.ttf");
+        ((TextView) findViewById(R.id.pillnr)).setTypeface(light);
 
         JessicaFetcher fetcher = new JessicaFetcher("123");
         Thread thread = new Thread(fetcher);
         thread.start();
-        while (fetcher.Data == null) {}
+        while (fetcher.Data == null) {} // User info stored in fetcher.Data (JSONObject)
+        displayInfo(fetcher.Data);
+    }
 
-        TextView topLabel = (TextView) findViewById(R.id.topLabel);
-        TextView pillnr = (TextView) findViewById(R.id.pillnr);
+    void displayInfo(JSONObject info) {
+        TextView description = (TextView) findViewById(R.id.description);
+        TextView pillNr = (TextView) findViewById(R.id.pillnr);
         TextView nextPill = (TextView) findViewById(R.id.nextpill);
-        pillnr.setTypeface(light);
+        TextView timestamp1 = (TextView) findViewById(R.id.timestamp1);
+        TextView event1 = (TextView) findViewById(R.id.event1);
+        TextView timestamp2 = (TextView) findViewById(R.id.timestamp2);
+        TextView event2 = (TextView) findViewById(R.id.event2);
+        TextView timestamp3 = (TextView) findViewById(R.id.timestamp3);
+        TextView event3 = (TextView) findViewById(R.id.event3);
+        TextView timestamp4 = (TextView) findViewById(R.id.timestamp4);
+        TextView event4 = (TextView) findViewById(R.id.event4);
+        TextView timestamp5 = (TextView) findViewById(R.id.timestamp5);
+        TextView event5 = (TextView) findViewById(R.id.event5);
 
         try {
-            topLabel.setText(fetcher.Data.getString("description"));
-            pillnr.setText(String.valueOf(fetcher.Data.getInt("pillCount")));
-            int minutesLeft = fetcher.Data.getInt("nextPillTime");
+            description.setText(info.getString("description"));
 
+            pillNr.setText(String.valueOf(info.getInt("pillCount")));
+
+            int minutesLeft = info.getInt("nextPillTime");
             if(minutesLeft < 60)
                 nextPill.setText(String.format(getString(R.string.next_pill_min), minutesLeft));
             else {
@@ -40,6 +57,18 @@ public class Status extends AppCompatActivity {
                     hoursLeft++;
                 nextPill.setText(String.format(getString(R.string.next_pill_hrs), hoursLeft));
             }
+
+            JSONArray array = info.getJSONArray("recent");
+            timestamp1.setText(array.getJSONArray(0).getString(0));
+            event1.setText(array.getJSONArray(0).getString(1));
+            timestamp2.setText(array.getJSONArray(1).getString(0));
+            event2.setText(array.getJSONArray(1).getString(1));
+            timestamp3.setText(array.getJSONArray(2).getString(0));
+            event3.setText(array.getJSONArray(2).getString(1));
+            timestamp4.setText(array.getJSONArray(3).getString(0));
+            event4.setText(array.getJSONArray(3).getString(1));
+            timestamp5.setText(array.getJSONArray(4).getString(0));
+            event5.setText(array.getJSONArray(4).getString(1));
         }
         catch (Exception e) {
             e.printStackTrace();

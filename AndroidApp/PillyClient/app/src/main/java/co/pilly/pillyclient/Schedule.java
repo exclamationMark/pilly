7dff44113cd5f7705f27a49b8ed5416492ec6b82
Davide <dabongio93@yahoo.it>
2016-07-23 18:46:01 +0200

    Added menu icons

--- AndroidApp/PillyClient/app/src/main/java/co/pilly/pillyclient/Schedule.java
+++ AndroidApp/PillyClient/app/src/main/java/co/pilly/pillyclient/Schedule.java
@@ -3,6 +3,8 @@ package co.pilly.pillyclient;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.ListView;
 
 public class Schedule extends AppCompatActivity {
@@ -13,6 +15,7 @@ public class Schedule extends AppCompatActivity {
         setContentView(R.layout.activity_schedule);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        myToolbar.setTitle("Schedule");
         setSupportActionBar(myToolbar);
 
         ListView listView = (ListView) findViewById(R.id.schedule_list);
@@ -21,5 +24,12 @@ public class Schedule extends AppCompatActivity {
         alerts[1] = new PillAlert(21, 30, 2, new DAYS[] {DAYS.MON, DAYS.WED, DAYS.FRI, DAYS.SAT, DAYS.SUN});
         ScheduleAdapter scheduleAdapter = new ScheduleAdapter(this, R.layout.list_element, alerts);
         listView.setAdapter(scheduleAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_schedule, menu);
+        return true;
     }
 }

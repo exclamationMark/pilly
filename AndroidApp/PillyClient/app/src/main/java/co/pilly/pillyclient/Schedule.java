e71be5ed7778ced3dd9d1bc0425844ce378913b5
Davide <dabongio93@yahoo.it>
2016-07-23 21:58:17 +0200

    Added long-press context menu

--- AndroidApp/PillyClient/app/src/main/java/co/pilly/pillyclient/Schedule.java
+++ AndroidApp/PillyClient/app/src/main/java/co/pilly/pillyclient/Schedule.java
@@ -2,9 +2,11 @@ package co.pilly.pillyclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ActionMode;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -32,6 +34,9 @@ public class Schedule extends AppCompatActivity {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
                 view.setSelected(true);
+                if(mActionMode != null)
+                    return false;
+                mActionMode = startSupportActionMode(mActionModeCallback);
                 return true;
             }
         });
@@ -43,4 +48,30 @@ public class Schedule extends AppCompatActivity {
         menuInflater.inflate(R.menu.menu_schedule, menu);
         return true;
     }
+
+    private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.context_schedule, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            mActionMode = null;
+        }
+    };
+
+    ActionMode mActionMode;
 }
